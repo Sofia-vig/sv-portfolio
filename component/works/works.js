d5af7c2f182d2ed6b41b8d72a92f21f127fe7137
sofia <sofiavign@gmail.com>
2021-06-09 02:18:03 -0300

    portfolio listo

---
+++ component/works/works.js
@@ -0,0 +1,20 @@
+function workComponent(sectionEl, info) {
+  const itemServiceElement = document.createElement("div");
+  itemServiceElement.classList.add("service");
+  itemServiceElement.innerHTML = `
+      <div class="service__item">
+        <img
+          src="${info.img}"
+          alt="camara-de-fotos"
+          class="service__image"
+        />
+        <h3 class="service__foot-image">${info.title}</h3>
+        <p class="service__ paragraph">
+         ${info.subtitle}
+        </p>
+        <a href="${info.link}" class="service__link">GitHub</a>
+      </div>    
+  `;
+
+  sectionEl.appendChild(itemServiceElement);
+}
--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -3,19 +3,33 @@ function components() {
   const sectionELementNav = document.querySelector(".container-principal");
   navComponent(sectionELementNav);
 
-  //SERVICE
-  const divElementService = document.querySelector(".service-container");
-  for (let i = 0; i < 5; i++) {
-    serviceComponent(divElementService);
-  }
-
   //FOOTER
   const divElementFooter = document.querySelector(".footer-container");
   footerComponent(divElementFooter);
 }
 
+//WORKS
+function getWorks() {
+  const divElementService = document.querySelector(".service-container");
+  fetch(
+    "https://cdn.contentful.com/spaces/ibdptpyrbdej/environments/master/entries?access_token=z9Me_ot5ZG8xZjmsDWjH3yXP6gmtr-ggAW8HvHR2hhE&content_type=portfolio"
+  ).then((response) => {
+    response.json().then((data) => {
+      data.items.forEach((item) => {
+        const object = {
+          title: item.fields.title,
+          subtitle: item.fields.subtitle,
+          img: item.fields.img,
+          link: item.fields.link,
+        };
+        workComponent(divElementService, object);
+      });
+    });
+  });
+}
 function main() {
   components();
+  getWorks();
 }
 
 main();
