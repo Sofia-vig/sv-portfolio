34faa167e54a3a599d8042c3502935682109c647
sofia <sofiavign@gmail.com>
2021-06-14 13:12:47 -0300

    nuevo nav bar , falta transition

--- component/nav/nav.js
+++ component/nav/nav.js
@@ -9,7 +9,12 @@ function navComponent(elementHtml) {
   <div></div>
 </div>
 <nav class="nav-bar">
-  <button class="close">X</button>
+  <a href="portfolio.html">Portfolio</a>
+  <a href="servicios.html">Servicios</a>
+  <a href="contacto.html">Contacto</a>
+</nav>
+<nav class="nav-bar-mobile">
+  <button class="nav-bar-mobile__close">X</button>
   <a href="portfolio.html">Portfolio</a>
   <a href="servicios.html">Servicios</a>
   <a href="contacto.html">Contacto</a>
@@ -21,12 +26,10 @@ function navComponent(elementHtml) {
 
 function burgerMobile() {
   const burger = document.querySelector(".nav__burger");
-  const nav = document.querySelector(".nav-bar");
-  const close = document.querySelector(".close");
+  const nav = document.querySelector(".nav-bar-mobile");
+  const close = document.querySelector(".nav-bar-mobile__close");
   burger.addEventListener("click", () => {
-    nav.classList.add("nav-mobile");
     nav.style.display = "flex";
-    close.style.display = "block";
     close.addEventListener("click", () => {
       nav.style.display = "none";
     });
